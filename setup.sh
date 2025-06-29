fe8a4942ae9c2d3fc8a024e1cb732ee2c2c85f97
satorukano <satorukano@satorukanonoMacBook-Air.local>
2025-06-29 09:50:52 +0200

    fix typo

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 module load singularity
-PROJECT_PATH=""
-TARGET_ROOT_PATH=""
+PROJECT_PATH="/work/satoru-k/projects/hbase"
+TARGET_ROOT_PATH="/work/satoru-k/projects/LogCoCo/output/hbase29"
 singularity exec LogCoCo_Java8.sif java -jar ./target/logcoco-findfileofclass-1.0-SNAPSHOT.jar "$PROJECT_PATH" "${TARGET_ROOT_PATH}/outputClass.txt" /opt/java/openjdk/jre/lib
-singularity exec LogCoCo_Java8.sif java -jar ./target/logcoco-fextractmethodcalmap-1.0-SNAPSHOT.jar "$PROJECT_PATH" "${TARGET_ROOT_PATH}/outputClass.txt" /opt/java/openjdk/jre/lib "${TARGET_ROOT_PATH}/outputContainLogMethodList.txt" "${TARGET_ROOT_PATH}/invoke_method.txt"
+singularity exec LogCoCo_Java8.sif java -jar ./target/logcoco-extractmethodcalmap-1.0-SNAPSHOT.jar "$PROJECT_PATH" "${TARGET_ROOT_PATH}/outputClass.txt" /opt/java/openjdk/jre/lib "${TARGET_ROOT_PATH}/outputContainLogMethodList.txt" "${TARGET_ROOT_PATH}/invoke_method.txt"
