6f3811f8cc6a09738e34d3678aaaf5c37392fa17
satorukano <satorukano@satorukanonoMacBook-Air.local>
2025-06-29 10:19:38 +0200

    fix args in setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ module load singularity
 PROJECT_PATH="/work/satoru-k/projects/hbase"
 TARGET_ROOT_PATH="/work/satoru-k/projects/LogCoCo/output/hbase29"
 singularity exec LogCoCo_Java8.sif java -jar ./target/logcoco-findfileofclass-1.0-SNAPSHOT.jar "$PROJECT_PATH" "${TARGET_ROOT_PATH}/outputClass.txt" /opt/java/openjdk/jre/lib
-singularity exec LogCoCo_Java8.sif java -jar ./target/logcoco-extractmethodcallmap-1.0-SNAPSHOT.jar "$PROJECT_PATH" "${TARGET_ROOT_PATH}/outputClass.txt" /opt/java/openjdk/jre/lib "${TARGET_ROOT_PATH}/outputContainLogMethodList.txt" "${TARGET_ROOT_PATH}/invoke_method.txt"
+singularity exec LogCoCo_Java8.sif java -jar ./target/logcoco-extractmethodcallmap-1.0-SNAPSHOT.jar "$PROJECT_PATH" "${TARGET_ROOT_PATH}/outputClass.txt" /opt/java/openjdk/jre/lib "${TARGET_ROOT_PATH}/outputContainLogMethodList.txt" "${TARGET_ROOT_PATH}/invoke_method.txt" org.apache.hadoop
