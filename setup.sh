5a0e51a561f71c424bf130c45df20ac62485eaa5
Kano Satoru <kiritomo0206@icloud.com>
2025-10-27 14:58:00 +0900

    change sif file name

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ module load singularity
 PROJECT="druid"
 PROJECT_PATH="/work/satoru-k/projects/druid"
 TARGET_ROOT_PATH="/work/satoru-k/projects/LogCoCo/output/druid44"
-singularity exec LogCoCo_Java17.sif java -jar ./target/logcoco-findfileofclass-1.0-SNAPSHOT.jar "$PROJECT_PATH" "${TARGET_ROOT_PATH}/outputClass.txt" 
-singularity exec LogCoCo_Java17.sif java -jar ./target/logcoco-extractmethodcallmap-1.0-SNAPSHOT.jar "$PROJECT_PATH" "${TARGET_ROOT_PATH}/outputClass.txt" "${TARGET_ROOT_PATH}/outputContainLogMethodList.txt" "${TARGET_ROOT_PATH}/invoke_method.txt" org.apache.druid
+singularity exec Java_17.sif java -jar ./target/logcoco-findfileofclass-1.0-SNAPSHOT.jar "$PROJECT_PATH" "${TARGET_ROOT_PATH}/outputClass.txt" 
+singularity exec Java_17.sif java -jar ./target/logcoco-extractmethodcallmap-1.0-SNAPSHOT.jar "$PROJECT_PATH" "${TARGET_ROOT_PATH}/outputClass.txt" "${TARGET_ROOT_PATH}/outputContainLogMethodList.txt" "${TARGET_ROOT_PATH}/invoke_method.txt" org.apache.druid
