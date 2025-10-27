90c4d57c2a8d056efb8b0d9a43fa555070105dcb
Kano Satoru <kiritomo0206@icloud.com>
2025-10-27 18:02:49 +0900

    並列ように変更

--- parallel_execution.sh
+++ parallel_execution.sh
@@ -15,5 +15,5 @@ LEAF_DIRECTORY_FILE_PATH="$TARGET_ROOT_DIR/leaf_directories.txt"
 TARGET_DIR=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$LEAF_DIRECTORY_FILE_PATH")
 TARGET_MODULE="org.apache.druid"
 PROJECT="druid"
-singularity exec Java_17.sif java -jar ./target/logcoco-preprocesslog-1.0-SNAPSHOT.jar "$PROJECT" "${TARGET_DIR}/log.txt" "${TARGET_DIR}" "${TARGET_ROOT_DIR}/outputClass.txt"
-singularity exec Java_17.sif java -Xms8G -Xmx64G -jar ./target/logcoco-mainparser-1.0-SNAPSHOT.jar "${TARGET_ROOT_DIR}/outputClass.txt" "${TARGET_DIR}/process_log.txt" "${TARGET_ROOT_DIR}/outputContainLogMethodList.txt" "${TARGET_DIR}/coverage.csv" "$TARGET_MODULE"
+singularity exec Java_17.sif java -Dlog4j2.debug=false -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -jar ./target/logcoco-preprocesslog-1.0-SNAPSHOT.jar "$PROJECT" "${TARGET_DIR}/log.txt" "${TARGET_DIR}" "${TARGET_ROOT_DIR}/outputClass.txt"
+singularity exec Java_17.sif java -Xms8G -Xmx64G -Dlog4j2.debug=false -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -jar ./target/logcoco-mainparser-1.0-SNAPSHOT.jar "${TARGET_ROOT_DIR}/outputClass.txt" "${TARGET_DIR}/process_log.txt" "${TARGET_ROOT_DIR}/outputContainLogMethodList.txt" "${TARGET_DIR}/coverage.csv" "$TARGET_MODULE"
